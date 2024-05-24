0601de4b1e82a694d30739fe28dc4de50e4617b3
guihhramos <guiramos902@gmail.com>
2024-05-24 18:01:14 -0300

    Lista 4 - FlexBox

---
+++ Listas/Lista5/js/ex02.js
@@ -0,0 +1,9 @@
+var nome = 'Guilherme'
+var idade = 18
+var empregoAtual = 'Analista de sistemas'
+var endereco = 'Vila Itaim'
+
+console.log(`Seu nome é ${nome}`)
+console.log(`Você tem ${idade}`)
+console.log(`Seu emprego atual é: ${empregoAtual}`)
+console.log(`E mora no endereço: ${endereco}`)
---
+++ Listas/Lista5/js/lista5.js
@@ -0,0 +1,10 @@
+var nome = prompt("Digite seu nome")
+var idade = 19
+var empregoAtual = 'Analista de sistemas'
+var endereco = 'Rua XPTO'
+
+alert(`Olá ${nome} \nTudo bem? `)
+alert(`Seu nome é ${nome}`)
+alert(`Você tem ${idade}`)
+alert(`Seu emprego atual é: ${empregoAtual}`)
+alert(`E mora no endereço: ${endereco}`)
