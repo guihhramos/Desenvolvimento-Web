16e8e3fcc3df0d3522bbd6608aada62edc9b72d8
guihhramos <guiramos902@gmail.com>
2024-05-31 14:25:45 -0300

    Lista 6

---
+++ Listas/Lista6/Lista6.js
@@ -0,0 +1,28 @@
+var pix = {
+    chavePix: '11963031279',
+    valorTransferencia: 1000.00,
+    dataTransacao: '31/05/24',
+    descricaoPagamento: 'descricao',
+    pagador: {
+        nome: 'Guilherme',
+        documento: '361.743.717-35'
+    },
+    recebedor: {
+        nome: 'Fulano',
+        documento: '135.268.144-53'
+    },
+    transacoes: [
+        {
+            data: '31/05/24',
+            valor: 800.00,
+            descricao: 'descricao'
+        },
+        {
+            data: '30/05/24',
+            valor: 199.99,
+            descricao: 'descricao'
+        }
+    ]
+}
+
+console.log(pix)
